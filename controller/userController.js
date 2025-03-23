ea59befd81e7fc6d7173daa97e3582015f46d7cd
sakshamgupta11 <sakshamgupta2308@gmail.com>
2025-03-23 17:42:20 +0530

    add login feature

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/user.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import dotenv from 'dotenv'
+import { error } from "console";
 dotenv.config()
 
 
@@ -24,20 +25,20 @@ class userConteroller {
                                 email: email,
                                 password: hashPassword,
                                 tc: tc
-        
+
                             })
-                           
+
                             const save_user = userModel.findOne({ email: email })
-                            const userSave =  await doc.save()
+                            const userSave = await doc.save()
                             // generate JWT token
                             const token = jwt.sign({ userID: save_user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' })
-                           
-                           await userModel.findByIdAndUpdate(userSave._id, { token });
+
+                            await userModel.findByIdAndUpdate(userSave._id, { token });
                             res.status(201).json({ status: "success", message: "registrered successfully", "token": token })
 
                         } catch (error) {
                             console.log(error);
-                         return   res.status(400).json({ error: "error in interting data registraction" })
+                            return res.status(400).json({ error: "error in interting data registraction" })
 
                         }
 
@@ -51,9 +52,50 @@ class userConteroller {
                 }
             }
         } catch (error) {
-
+res.status(500).json({ status: "failed", message: "Internal server error" })
         }
     }
+    static userLogin = async (req, res) => {
+        try {
+            // Extract email, password, and tc (terms & conditions) from the request body
+            const { email, password, tc } = req.body;
+    
+            // Validate if all required fields are provided
+            if (!email || !password || !tc) {
+                return res.status(400).json({ error: "All fields are required" }); // Fixed typo in "fields"
+            }
+    
+            // Find the user in the database using the provided email
+            const user = await userModel.findOne({ email: email });
+    
+            // If user does not exist, return an error response
+            if (!user) {
+                return res.status(400).json({ error: "User does not exist" });
+            }
+    
+            // Compare the provided password with the stored hashed password
+            const isMatch = await bcrypt.compare(password, user.password);
+    
+            // If password is incorrect, return an error response
+            if (!isMatch) {
+                return res.status(400).json({ error: "Email or password is incorrect" }); // Fixed typo in "incorrect"
+            }
+    
+            // Generate a JWT token with user ID and set expiration time to 5 days
+            const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '5d' });
+    
+            // Send success response along with the generated token
+            return res.status(200).json({ status: "success", message: "Login successful", token: token });
+    
+        } catch (error) {
+            // Log the error in the console for debugging
+            console.error(error);
+    
+            // Send an internal server error response
+            return res.status(500).json({ status: "failed", message: "Internal server error" }); // Fixed typo in "message" & "internal"
+        }
+    }
+    
 }
 
 export default userConteroller
