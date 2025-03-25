891cce6866490e2b3f43a6d0d9e061bcfb103c99
sakshamgupta11 <sakshamgupta2308@gmail.com>
2025-03-26 04:56:46 +0530

    Created API FOR RESET password and send email with  reeset usrl

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,6 +12,8 @@ router.use('/loginuser',checkUserAuth)
 //public route 
 router.post('/register',userConteroller.userRegistration)
 router.put('/login',userConteroller.userLogin)
+router.post('/send-reset-password-email',userConteroller.sendUserPasswordResetEmail)
+router.post('/reset-password/:id/:token',userConteroller.userPasswordReset)
 // private route
 router.post('/updatepassword',userConteroller.changePassword)
 router.get("/loginuser",userConteroller.loggedUser)
